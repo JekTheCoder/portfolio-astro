28e4563900bd315b674110f4c9829dbf3ca57fad
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-06-12 14:48:56 -0500

    feat: display icon to close dialog

--- src/components/CloseDialogButton.tsx
+++ src/components/CloseDialogButton.tsx
@@ -1,7 +1,8 @@
 import { closeDialog } from "@/lib/state/project-dialog";
+import Icon from "./Icon";
 
 export default function CloseDialogButton() {
-	return <button class="rounded-full p-2 bg-opacity-20 hover:bg-zinc-700" onClick={closeDialog}>
-		x
+	return <button class="rounded-full grid place-content-center h-8 w-8 hover:bg-opacity-20 hover:bg-white text-bold" onClick={closeDialog}>
+		<Icon name="minus" class="h-4 w-4" />
 	</button>
 }
